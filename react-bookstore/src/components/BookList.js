9b41111950fc2bcaf452f3e655d9aff1b29f0eae
Mukesh K <mukesh.kumar.me.2018@miet.ac.in>
2024-09-04 16:24:46 +0530

    modified

---
+++ react-bookstore/src/components/BookList.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function BookList() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/books")
+      .then((response) => {
+        setBooks(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the books!", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Book List</h2>
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>
+            {book.title} by {book.author}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default BookList;

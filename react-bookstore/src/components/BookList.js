8205bdf15f378a5f0a2ba1875503fd852eb2d34e
Mukesh K <mukesh.kumar.me.2018@miet.ac.in>
2024-09-05 13:01:01 +0530

    http methods add to booklist,js

--- react-bookstore/src/components/BookList.js
+++ react-bookstore/src/components/BookList.js
@@ -3,8 +3,15 @@ import axios from "axios";
 
 function BookList() {
   const [books, setBooks] = useState([]);
+  const [newBook, setNewBook] = useState({ title: "", author: "" });
+  const [editBook, setEditBook] = useState(null);
+  const [updateDetails, setUpdateDetails] = useState({ title: "", author: "" });
 
   useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = () => {
     axios
       .get("http://localhost:8080/api/books")
       .then((response) => {
@@ -13,7 +20,43 @@ function BookList() {
       .catch((error) => {
         console.error("There was an error fetching the books!", error);
       });
-  }, []);
+  };
+
+  const handleCreateBook = () => {
+    axios
+      .post("http://localhost:8080/api/books", newBook)
+      .then(() => {
+        setNewBook({ title: "", author: "" });
+        fetchBooks();
+      })
+      .catch((error) => {
+        console.error("There was an error creating the book!", error);
+      });
+  };
+
+  const handleUpdateBook = (id) => {
+    axios
+      .put(`http://localhost:8080/api/books/${id}`, updateDetails)
+      .then(() => {
+        setEditBook(null);
+        setUpdateDetails({ title: "", author: "" });
+        fetchBooks();
+      })
+      .catch((error) => {
+        console.error("There was an error updating the book!", error);
+      });
+  };
+
+  const handleDeleteBook = (id) => {
+    axios
+      .delete(`http://localhost:8080/api/books/${id}`)
+      .then(() => {
+        fetchBooks();
+      })
+      .catch((error) => {
+        console.error("There was an error deleting the book!", error);
+      });
+  };
 
   return (
     <div>
@@ -22,9 +65,45 @@ function BookList() {
         {books.map((book) => (
           <li key={book.id}>
             {book.title} by {book.author}
+            <button onClick={() => setEditBook(book)}>Edit</button>
+            <button onClick={() => handleDeleteBook(book.id)}>Delete</button>
           </li>
         ))}
       </ul>
+
+      <h3>Add New Book</h3>
+      <input
+        type="text"
+        placeholder="Title"
+        value={newBook.title}
+        onChange={(e) => setNewBook({ ...newBook, title: e.target.value })}
+      />
+      <input
+        type="text"
+        placeholder="Author"
+        value={newBook.author}
+        onChange={(e) => setNewBook({ ...newBook, author: e.target.value })}
+      />
+      <button onClick={handleCreateBook}>Add Book</button>
+
+      {editBook && (
+        <div>
+          <h3>Edit Book</h3>
+          <input
+            type="text"
+            placeholder="Title"
+            value={updateDetails.title}
+            onChange={(e) => setUpdateDetails({ ...updateDetails, title: e.target.value })}
+          />
+          <input
+            type="text"
+            placeholder="Author"
+            value={updateDetails.author}
+            onChange={(e) => setUpdateDetails({ ...updateDetails, author: e.target.value })}
+          />
+          <button onClick={() => handleUpdateBook(editBook.id)}>Update Book</button>
+        </div>
+      )}
     </div>
   );
 }
